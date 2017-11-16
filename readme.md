7242ea5f1121b95034dc6031c2961963b07e40cb
Luke Sweeney <luke@thesweeneys.org>
2017-11-16 15:40:25 -0600

    Initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Save a Table Client
+This is a small script that detects when a physical switch is flipped, and when it is, POSTs it's state to the Save a Table site. the table ID will be stored in a file called `~/table_id`. This allows the code to be the same for every client, but they still need unique IDs.
+
+I'm also logging every time a request is made. It's only a few extra lines of code, and I'll get some data.
---
+++ save_a_table_client.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import time
+from os.path import expanduser
+
+table_data_file = expanduser("~") + "/table_data"
+
+url = 'http://ec2-54-200-137-241.us-west-2.compute.amazonaws.com:5000/update-table'
+
+table = json.loads(open(table_data_file, 'r').read())
+
+switch_state = 1
+
+data = {
+    'table': {
+        'number': table['number'],
+        'floor': table['floor'],
+        'state': switch_state # Get this from the switch
+    }
+}
+
+def post_data(url, data):
+    request = requests.post(url, headers={'Content-Type': 'application/json'},  data=json.dumps(data))
+    return request.text
+
+def log_response(response, posted_data):
+    with open(expanduser("~") + "/table_log", 'w') as file:
+        file.write("Request made at " + time.strftime("%H:%M:%S") + " on " + time.strftime("%d/%m/%Y") )
+        file.write("\n")
+        file.write("posted_data: ")
+        file.write(str(data))
+        file.write("\n")
+        file.write("Response: " + response)
+        file.write("\n")
+        file.write("\n")
+
+
+response = post_data(url, data)
+log_response(response, data)
