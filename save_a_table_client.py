488db39fa49894e7223a95f13c0bc666de48fe15
Luke Sweeney <luke@thesweeneys.org>
2017-11-17 22:16:02 -0600

    Adds error catching for when the server is down

--- save_a_table_client.py
+++ save_a_table_client.py
@@ -6,13 +6,18 @@ import RPi.GPIO as GPIO
 
 path = os.path.dirname(os.path.abspath(__file__))
 
+GPIO.setwarnings(False)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(5, GPIO.IN)
 GPIO.setup(22, GPIO.OUT)
+
 old_button_status = GPIO.input(5)
 
 
-url = 'http://ec2-54-200-137-241.us-west-2.compute.amazonaws.com:5000/update-table'
+# url = 'http://ec2-54-200-137-241.us-west-2.compute.amazonaws.com:5000/update-table'
+url = 'http://192.168.0.59:5000/update-table'
+
 
 table = json.loads(open(path + "/table_data", 'r').read())
 
@@ -43,16 +48,26 @@ def log_response(response, posted_data):
         file.write("\n")
         file.write("\n")
 
+def write_log_failure(err):
+    with open(path + "/table_log", 'a') as file:
+        file.write("FAILURE: Failed request made at " + time.strftime("%H:%M:%S") + " on " + time.strftime("%d/%m/%Y"))
+        file.write("\n")
+        file.write("Is the server down? I think it's probably down... check it out. If you're not Luke, call Luke.\n")
+        file.write("--- Exception ---\n")
+        file.write(str(err))
+        file.write("\n")
+        file.write("\n")
 
 # Detect switch loop, post data when flipped
 while True:
     button_status = GPIO.input(5)
-    # Don't know if this is necessary...
-    # GPIO.output(22, button_status)
     if old_button_status != button_status:
         data['table']['state'] = button_status
-        response = post_data(url, data)
-        log_response(response, data)
+        try:
+            response = post_data(url, data)
+            log_response(response, data)
+        except requests.exceptions.ConnectionError as err:
+            write_log_failure(err)
         old_button_status = button_status
     time.sleep(0.2)
 GPIO.cleanup()
